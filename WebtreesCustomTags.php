64d5ce009ce7a530e96aa7d4e0a6c917bb154399
Ekrem Parlak <ekremparlak@outlook.com>
2022-07-03 12:36:15 +0300

    ...

--- WebtreesCustomTags.php
+++ WebtreesCustomTags.php
@@ -106,7 +106,7 @@ class WebtreesCustomTags extends AbstractModule implements ModuleCustomInterface
             case 'tr':
             case 'tr-TR':
                 return [
-                    'Marriage Name' => 'Evlilik Soyadı',
+                    'Marriage Surname' => 'Evlilik Soyadı',
                 ];
 
             default:
